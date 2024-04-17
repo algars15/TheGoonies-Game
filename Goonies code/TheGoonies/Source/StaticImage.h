9d807df69eb2363440272fe3721b3ab76d436020
Cai128 <160216293+Cai128@users.noreply.github.com>
2024-04-17 11:01:23 +0200

    Update StaticImage.h

--- Goonies code/TheGoonies/Source/StaticImage.h
+++ Goonies code/TheGoonies/Source/StaticImage.h
@@ -1,5 +1,18 @@
 #pragma once
-class StaticImage
+#include "RenderComponent.h"
+
+class StaticImage : public RenderComponent
 {
+public:
+    StaticImage(const Texture2D* texture, const Rectangle& rect);
+    ~StaticImage();
+
+    void Draw(int x, int y) const override;
+    void DrawTint(int x, int y, const Color& col) const override;
+    void Release() override;
+
+private:
+    Rectangle rc;
+    const Texture2D* img;
 };
 
